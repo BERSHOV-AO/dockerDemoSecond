0aa55698779b37c98f9195dabaf33e8c84a97e11
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-11-13 09:53:08 +0300

    Initial commit + README

---
+++ src/test/java/ru/netology/dockerdemosecond/DockerDemoSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.dockerdemosecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
